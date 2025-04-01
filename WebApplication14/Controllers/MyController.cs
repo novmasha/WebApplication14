3dc3e45ed1c29153c1dee18a1f9efce9bc88ab53
novmasha <novmasha@bk.ru>
2025-04-01 12:50:36 +0300

    Добавьте файлы проекта.

---
+++ WebApplication14/Controllers/MyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApplication14.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from the API!");
+        }
+    }
+}
